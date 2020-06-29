5b4c6911c9a2b6c94a819ae4cac73c16506a4eca
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-06-29 17:51:36 +0000

    Automatically backed up by Learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -24,19 +24,51 @@ class LoginForm extends React.Component {
     this.props.handleLogin(this.state)
   }
 
+
+handleLogin = event => {
+  this.setState({
+    username: '',
+    password: ''
+  })
+}
+
+handleChange = event => {
+  this.setState({
+    [event.target.name]: event.target.value
+  })
+}
+
+handleSubmit = event => {
+  event.preventDefault()
+  if (!this.state.username || !this.state.password)
+  return 
+    this.props.onSubmit(this.state)
+  })
+}
+
+
+
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
+<<<<<<< HEAD
             <input id="username" name="username" type="text" onChange={this.handleChange} />
+=======
+            <input id="username" name="username" type="text" onChange={this.handleChange}/>
+>>>>>>> 6d8e00e88639916d13ab780d4d2944b0c05981eb
           </label>
         </div>
         <div>
           <label>
             Password
+<<<<<<< HEAD
             <input id="password" name="password" type="password" onChange={this.handleChange} />
+=======
+            <input id="password" name="password" type="password" onChange={this.handleChange}/>
+>>>>>>> 6d8e00e88639916d13ab780d4d2944b0c05981eb
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -12,11 +12,18 @@ class TwitterMessage extends React.Component {
   render() {
     return (
       <div>
+<<<<<<< HEAD
         <strong>Your message:</strong>
         <input type="text" 
                onChange={event => this.setState({message: event.target.value})}
                value ={this.state.message}/>
                {this.props.maxChars-this.state.message.length}      
+=======
+       <input type="text" 
+               onChange={event => this.setState({message: event.target.value})}
+               value ={this.state.message}/>
+               {this.props.maxChars-this.state.message.length}   
+>>>>>>> 6d8e00e88639916d13ab780d4d2944b0c05981eb
       </div>
     );
   }
